70a91f21fa2f27bf912e9961cd31fcf953d773ee
SergeyKulagin <kulaginsp@gmail.com>
2019-03-17 12:02:35 +0300

    [ITA]: merge sort (prepare for it)

--- introduction-to-algorithms/2/3/merge-sort.py
+++ introduction-to-algorithms/2/3/merge-sort.py
@@ -2,24 +2,18 @@ def merge(a, start, middle, end):
     temp = []
     i = start
     j = middle
-    for k in range (start, end):
+    for k in range(start, end):
         if a[i] <= a[j]:
             temp.append(a[i])
             i = i + 1
         else:
             temp.append(a[j])
             j = j + 1
-
+    # todo the last element?
     temp.reverse()
-    for m in range (start, end):
+    for m in range(start, end):
         a[m] = temp.pop()
     return a
 
 
-print(merge([1,3,5,2,4], 0, 3, 4))
-
-
-
-
-
-
+print(merge([1, 3, 5, 2, 4], 0, 3, 4))

fe706daceeecf378d9121cda01fd1575f34e7ee0
sergey.kulagin <kulaginsp@gmail.com>
2019-03-21 13:31:50 +0300

    [ITA]: exercises 2.3 - insertion sort recursive

--- introduction-to-algorithms/2/insertion-sort.py
+++ introduction-to-algorithms/2/insertion-sort.py
@@ -19,6 +19,28 @@ def insertionSortDesc(a):
         a[i + 1] = key
     return a
 
+# 2.3-4
+def insertionSortRec(a):
+    insertionSortRecIt(a, len(a) - 1)
+    return a
+
+
+def insertIntoSorted(a, idx):
+    el = a[idx]
+    insertIdx = idx
+    for i in range(idx - 1, -1, -1):
+        if el < a[i]:
+            a[i + 1] = a[i]
+            insertIdx = insertIdx - 1
+    a[insertIdx] = el
+
+
+def insertionSortRecIt(a, i):
+    if i == 0:
+        return
+    insertionSortRecIt(a, i - 1)
+    insertIntoSorted(a, i)
 
 print(insertionSortAsc([2, 3, 1]))
-print(insertionSortDesc([2,3,1]))
+print(insertionSortDesc([2, 3, 1]))
+print(insertionSortRec([5,2,0,7,4,5]))

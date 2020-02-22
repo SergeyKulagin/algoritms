95024e24ce38650da96fc7363cb66e0c0b5030a3
sergey.kulagin <sergey.kulagin@synesis-sport.by>
2020-02-22 14:48:30 +0300

    [ITA]: quicksort with equal elements - multipartition method

--- introduction-to-algorithms/7-quicksort/quicksort.py
+++ introduction-to-algorithms/7-quicksort/quicksort.py
@@ -26,7 +26,7 @@ def quickSort(A):
     quickSortStep(A, 0, len(A) - 1)
     return A
 
-#Hoare partition
+# Hoare partition
 def hoare_partition(A, p, r):
     x = A[p]
     i = p - 1
@@ -60,9 +60,33 @@ def quickSortHoare(A):
     quickSortStepHoare(A, 0, len(A) - 1)
     return A
 
+# todo double check and test carefully
+# multi partition
+def multiPartition(A, p, r):
+    x = A[p]
+    i = p - 1
+    j = p + 1
+    k = p
+    while j < r + 1:
+        if A[j] < x:
+            i = i + 1
+            A[i] = A[j]
+            k = k + 1
+            A[j] = A[k]
+            A[k] = x
+        if A[j] == x:
+            k = k + 1
+            A[j] = A[k]
+            A[k] = x
+        j = j + 1
+    return (A, (i + 1, k))
 
-print(quickSort([2, 7, 10, 11, 3, 0]))
-print(quickSort([14, 24, 46, 49, 96, 11, 17, 99, 3, 33, 41, 6, 51, 30, 31]))
 
-print(quickSortHoare([2, 7, 10, 11, 3, 0]))
-print(quickSortHoare([14, 24, 46, 49, 96, 11, 17, 99, 3, 33, 41, 6, 51, 30, 31]))
+# print(quickSort([2, 7, 10, 11, 3, 0]))
+# print(quickSort([14, 24, 46, 49, 96, 11, 17, 99, 3, 33, 41, 6, 51, 30, 31]))
+#
+# print(quickSortHoare([2, 7, 10, 11, 3, 0]))
+# print(quickSortHoare([14, 24, 46, 49, 96, 11, 17, 99, 3, 33, 41, 6, 51, 30, 31]))
+
+l = [5, 7, 7, 10, 5, 5, 3, 2, 1, 5, 12, 0, 99, 2]
+print(multiPartition(l, 0, len(l) - 1))

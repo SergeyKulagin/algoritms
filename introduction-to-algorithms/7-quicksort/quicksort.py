2de81aad338fa8192e3aaadb995879725afb951c
sergey.kulagin <sergey.kulagin@synesis-sport.by>
2020-02-23 14:59:16 +0300

    [ITA]: quicksort - multipartition

--- introduction-to-algorithms/7-quicksort/quicksort.py
+++ introduction-to-algorithms/7-quicksort/quicksort.py
@@ -19,6 +19,7 @@ def partition(A, p, r):
     A[r] = t
     return i + 1
 
+
 def random_partition(A, p, r):
     random_pivot = randint(p, r)
     t = A[r]
@@ -74,6 +75,7 @@ def quickSortHoare(A):
     quickSortStepHoare(A, 0, len(A) - 1)
     return A
 
+
 # todo double check and test carefully
 # multi partition
 def multiPartition(A, p, r):
@@ -88,12 +90,24 @@ def multiPartition(A, p, r):
             k = k + 1
             A[j] = A[k]
             A[k] = x
-        if A[j] == x:
+        elif A[j] == x:
             k = k + 1
             A[j] = A[k]
             A[k] = x
         j = j + 1
-    return (A, (i + 1, k))
+    return (i + 1, k)
+
+
+def quickSortStepMultiPartition(A, p, r):
+    if p < r:
+        q = multiPartition(A, p, r)
+        quickSortStepMultiPartition(A, p, q[0] - 1)
+        quickSortStepMultiPartition(A, q[1] + 1, r)
+
+
+def quickSortMultiPartition(A):
+    quickSortStepMultiPartition(A, 0, len(A) - 1)
+    return A
 
 
 print(quickSort([2, 7, 10, 11, 3, 0]))
@@ -102,5 +116,10 @@ print(quickSort([14, 24, 46, 49, 96, 11, 17, 99, 3, 33, 41, 6, 51, 30, 31]))
 print(quickSortHoare([2, 7, 10, 11, 3, 0]))
 print(quickSortHoare([14, 24, 46, 49, 96, 11, 17, 99, 3, 33, 41, 6, 51, 30, 31]))
 
+print(quickSortMultiPartition([2, 7, 10, 11, 3, 0]))
+print(quickSortMultiPartition([14, 24, 46, 49, 96, 11, 17, 99, 3, 33, 41, 6, 51, 30, 31]))
+
 # l = [5, 7, 7, 10, 5, 5, 3, 2, 1, 5, 12, 0, 99, 2]
+# l = [18,3,3,3,3,1,1,1,1,7,7,7,7,12,12,17,17,18,0,0]
+# l = [2, 1, 0, 4, 5]
 # print(multiPartition(l, 0, len(l) - 1))

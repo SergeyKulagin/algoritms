1c08b4ee3dd986da7e8a6ccaece8c83a437bf8ab
sergey.kulagin <sergey.kulagin@synesis-sport.by>
2020-03-05 17:03:15 +0300

    [ITA]: quicksort - fuzzy interval sort - sort algorithm - bug fix - tests

--- introduction-to-algorithms/7-quicksort/fuzzyintervalsort.py
+++ introduction-to-algorithms/7-quicksort/fuzzyintervalsort.py
@@ -9,18 +9,23 @@ def intersect(i1, i2):
     B = i1[1]
     C = i2[0]
     D = i2[1]
+    assert A <= B, "interval is ({},{})".format(A, B)
+    assert C <= D, "interval is ({},{})".format(C, D)
     if C >= A and C <= B:
         return C, (D if D <= B else B)
     elif D >= A and D <= B:
         return A, D
+    elif C < A and D >= B:
+        return A, B
     return None
 
 
 def left(i): return i[0]
+
+
 def right(i): return i[1]
 
 
-# todo double check
 def partition(A, p, r):
     x = A[p]
     i = p - 1
@@ -43,9 +48,34 @@ def partition(A, p, r):
     return (A, (i + 1, k))
 
 
+# performs in O(n) if all intervals have common interval
+def fuzzy_sort(A):
+    fuzzy_sort_it(A, 0, len(A) - 1)
+    return A
+
+
+def fuzzy_sort_it(A, p, r):
+    if p < r:
+        pivot = partition(A, p, r)[1]
+        fuzzy_sort_it(A, p, left(pivot) - 1)
+        fuzzy_sort_it(A, right(pivot) + 1, r)
+
+
+def partition_(A):
+    return partition(A, 0, len(A) - 1)
+
+
 print(intersect((0, 1), (0.75, 2)))
 print(intersect((0, 1), (2, 3)))
 print(intersect((0, 2), (2, 3)))
 
-l = [(0, 5), (-1, 1), (0, 1), (0.5, 4), (1.5, 3), (-1, 0.25)]
-print(partition(l, 0, 5))
+print(partition_([(0, 5), (-1, 1), (0, 1), (0.5, 4), (1.5, 3), (-1, 0.25)]))
+print(partition_([(-11, -1), (10, 20), (1, 5)]))
+print(partition_([(1, 1), (-1, -1), (5, 5), (0, 0)]))
+
+print(fuzzy_sort([(0, 5), (-1, 1), (0, 1), (0.5, 4), (1.5, 3), (-1, 0.25)]))
+print(fuzzy_sort([(-11, -1), (10, 20), (1, 5)]))
+print(fuzzy_sort([(1, 1), (-1, -1), (5, 5), (0, 0)]))
+print(fuzzy_sort([(10, 11), (0, 5), (0, 3), (-5, -5), (0, 0), (32, 33)]))
+print(fuzzy_sort([(0, 10), (0, 1), (1, 2), (2, 4), (5, 6)]))
+print(fuzzy_sort([(3.5, 10), (3, 4), (0, 5), (2, 7)]))

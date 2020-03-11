c4cf3ba459c15c9b108c5d5b79027258e926c194
sergey.kulagin <sergey.kulagin@synesis-sport.by>
2020-03-11 19:04:40 +0300

    [leetcode]: findMedianSortedArrays (third approach - in progress)

--- leetcode-problems/medianSortedArrays.py
+++ leetcode-problems/medianSortedArrays.py
@@ -3,25 +3,30 @@ class Solution(object):
     def findMedianSortedArrays(self, nums1, nums2):
         (small, big) = (nums1, nums2) if len(nums1) < len(nums2) else (nums2, nums1)
         full_len = len(nums1) + len(nums2)
-        median_index = (full_len / 2, full_len / 2 - 1) if full_len % 2 == 0 else (int(full_len / 2), int(full_len / 2))
+        median_index = (full_len / 2 - 1, full_len / 2) if full_len % 2 == 0 else (int(full_len / 2), int(full_len / 2))
         if len(small) == 0:
             return (big[median_index[0]] + big[median_index[1]]) / 2
 
-        left = self.binarySearch_(big, small[0])[3]
-        right = self.binarySearch_(big, small[len(small) - 1])[2]
+        p = 0
+        r = len(small) - 1
+        while p <= r:
+            m = int((p + r) / 2)
+            res = Solution.binarySearch(big, 0, len(big) - 1, small[m])
+            cur_idx = res[2] + m + 1
+            if cur_idx < median_index[0]:
+                p = m + 1
+            else:
+                r = m - 1
 
-        if left > median_index[1]:
-            return (big[median_index[0]] + big[median_index[1]]) / 2
-        if right < median_index[0]:
-
-
-    def median(A, B): #todo
+        big_cursor = res[2]
+        small_cursor = m
+        # what to do next?
 
     def binarySearch(A, p, r, el):
         while p <= r:
             m = int((r + p) / 2)
             if A[m] == el:
-                return True, m, m - 1, m + 1
+                return True, m, m, m
             elif A[m] > el:
                 r = m - 1
                 left = r
@@ -36,7 +41,11 @@ class Solution(object):
         return Solution.binarySearch(A, 0, len(A) - 1, el)
 
 
-print(Solution.binarySearch_([1, 3, 5, 10], 7))
-print(Solution.binarySearch_([1, 3, 5, 10], 2))
-print(Solution.binarySearch_([1, 3, 5, 10], -1))
-print(Solution.binarySearch_([1, 3, 5, 10], 3))
+# print(Solution.binarySearch_([1, 3, 5, 10], 7))
+# print(Solution.binarySearch_([1, 3, 5, 10], 2))
+# print(Solution.binarySearch_([1, 3, 5, 10], -1))
+# print(Solution.binarySearch_([1, 3, 5, 10], 3))
+# print(Solution.binarySearch_([1, 3, 5, 10], 12))
+#Solution.findMedianSortedArrays(Solution, nums1=[1,2,5,7,10], nums2=[8,9])
+#Solution.findMedianSortedArrays(Solution, nums1=[1,2,5,7,10], nums2=[15,16])
+Solution.findMedianSortedArrays(Solution, nums1=[1,5,9,10,15,17,20], nums2=[6,11,16])

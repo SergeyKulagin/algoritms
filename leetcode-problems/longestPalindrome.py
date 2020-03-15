feca0fb35375671efbe2e3a2477bdcb4c13029ad
sergey.kulagin <sergey.kulagin@synesis-sport.by>
2020-03-15 11:32:46 +0300

    [leetcode]: Longest Palindromic Substring (adoptions for leetcode)

--- leetcode-problems/longestPalindrome.py
+++ leetcode-problems/longestPalindrome.py
@@ -1,33 +1,48 @@
-def longestPalindrome(s):
-    n = len(s) - 1
-    r = n
-    while r > 0:
-        i = 0
+class Solution(object):
+    def longestPalindrome(self, s):
+        if s == "":
+            return s
+        n = len(s) - 1
+        r = n
+        while r > 0:
+            i = 0
+            j = r
+            while j <= n:
+                palindrome = self.isPalindrome(s, i, j)
+                if palindrome:
+                    return s[i:j + 1]
+                i = i + 1
+                j = j + 1
+            r = r - 1
+        return s[0]
+
+    def isPalindrome(self, s, p, r):
+        i = p
         j = r
-        while j <= n:
-            print("Check " + s[i:j + 1])
-            palindrome = isPalindrome(s, i, j)
-            if palindrome:
-                return (i, j), s[i:j + 1]
+        while i <= r:
+            if s[i] != s[j]:
+                return False
             i = i + 1
-            j = j + 1
-        r = r - 1
-    return (0,0), s[0]
+            j = j - 1
+        return True
 
 
-def isPalindrome(s, p, r):
-    i = p
-    j = r
-    while i <= r:
-        if s[i] != s[j]:
-            return False
-        i = i + 1
-        j = j - 1
-    return True
+solution = Solution()
+#print(solution.longestPalindrome("babad"))
+print(solution.longestPalindrome("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabcaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"))
+#print(solution.longestPalindrome(""))
+#print(solution.longestPalindrome("cbbd"))
+#print(solution.longestPalindrome("hannah"))
+#print(solution.longestPalindrome("a"))
+#print(solution.longestPalindrome("abcabccba"))
 
+# "babad"
+# ""
+# "a"
+# "cbbd"
+# "hannah"
+# "abcabccba"
+# "redder"
 
-print(longestPalindrome("babad"))
-print(longestPalindrome("cbbd"))
-print(longestPalindrome("hannah"))
-print(longestPalindrome("a"))
-print(longestPalindrome("abcabccba"))
+#PROBLEM
+# aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabcaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

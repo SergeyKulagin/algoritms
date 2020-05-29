3b34b25fbd9095f1fcb7f2beddfc5c0f089dae48
sergey.kulagin <sergey.kulagin@synesis-sport.by>
2020-05-29 18:22:18 +0300

    [leetcode]: regexp matching (in progress) - handle more cases

--- leetcode-problems/regexMatch.py
+++ leetcode-problems/regexMatch.py
@@ -1,3 +1,5 @@
+# see also https://regex101.com/
+
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         s_idx = 0
@@ -10,8 +12,16 @@ class Solution:
                 p_idx += 1
             elif p[p_idx] == '*':
                 if p_idx == 0:
-                    raise RuntimeError('Invalid pattern detected')
-                elif p[p_idx - 1] == '.' or p[p_idx - 1] == s[s_idx]:
+                    raise RuntimeError('Invalid pattern')
+                elif p[p_idx - 1] == '.':
+                    if p_idx < p_len - 1 and p[p_idx + 1] == s[s_idx]:
+                        s_idx += 1
+                        p_idx += 1
+                    # elif p_idx < p_len - 1 and p[p_idx + 1] == '.':
+                    #     p_idx += 1
+                    else:
+                        s_idx += 1
+                elif p[p_idx - 1] == s[s_idx]:
                     s_idx += 1
                 else:
                     p_idx += 1
@@ -24,12 +34,31 @@ class Solution:
                 else:
                     return False
 
-        return True if (s_idx >= s_len and p_idx >= p_len - 1) else False
+        return s_idx >= s_len and (p_idx >= p_len - 1 or self.emptyExpressionLeft(p, p_idx, len(p)))
+
+    def emptyExpressionLeft(self, p: str, start: int, end: int):
+        while start < end:
+            if p[start] == '*':
+                if p[start - 1] == '*':
+                    raise RuntimeError('Invalid pattern')
+                start += 1
+            elif start < end - 1 and p[start + 1] == '*':
+                start += 1
+            else:
+                return False
+
+    def isMatchWithPrint(self, s: str, p: str) -> bool:
+        print(s + " is mathes to " + p + " = " + str(self.isMatch(s, p)))
 
 
 s = Solution()
-print(s.isMatch("aa", "a"))
-print(s.isMatch("aa", "a*"))
-print(s.isMatch("ab", ".*"))
-print(s.isMatch("aab", "c*a*b"))
-print(s.isMatch("mississippi", "mis*is*p*."))
+# s.isMatchWithPrint("1", '1*')
+# s.isMatchWithPrint("1", '1.*')
+# s.isMatchWithPrint("aa", "a")
+# s.isMatchWithPrint("aa", "a*")
+# s.isMatchWithPrint("ab", ".*")
+# s.isMatchWithPrint("aab", "c*a*b")
+# s.isMatchWithPrint("mississippi", "mis*is*p*.")
+s.isMatchWithPrint("aaaaaaaaaaaaabq", ".*.q")
+# s.isMatchWithPrint("abc", ".*.*.*")
+# s.isMatchWithPrint("hello", ".*")

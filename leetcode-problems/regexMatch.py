14554231638f18fe12a73be4900c127fc88f62b0
sergey.kulagin <sergey.kulagin@synesis-sport.by>
2020-07-23 17:52:50 +0300

    [leetcode]: regexp matching - improve overall logic (1 case handle)

--- leetcode-problems/regexMatch.py
+++ leetcode-problems/regexMatch.py
@@ -6,35 +6,54 @@ class Solution:
         p_idx = 0
         s_len = len(s)
         p_len = len(p)
-        while s_idx < s_len and p_idx < p_len:
-            if p[p_idx] == '.':
-                s_idx += 1
+        p_context_cur = self.get(p, 0)
+        p_context_prev = None
+        while s_idx < s_len and p_context_cur is not None:
+            if p_context_cur == '.':
                 p_idx += 1
-            elif p[p_idx] == '*':
-                if p_idx == 0:
+                s_idx += 1
+                p_context_prev = p_context_cur
+                p_context_cur = self.get(p, p_idx)
+            elif p_context_prev == '.' and p_context_cur == '*':  # handling '.*'
+                while self.get(p, p_idx + 1) == '.' and self.get(p, p_idx + 2) == '*':
+                    p_idx += 2
+                while self.get(p, p_idx + 1) == '.':
+                    p_idx += 1
+
+                p_idx += 1
+                if self.get(p, p_idx) == s[s_idx]:
+                    p_idx += 1
+                    s_idx += 1
+                    p_context_cur = self.get(p, p_idx)
+                    p_context_prev = self.get(s, s_idx)
+                else:
+                    s_idx += 1
+            elif p_context_cur == '*':
+                if p_idx == 0 or p_context_prev == '*':
                     raise RuntimeError('Invalid pattern')
-                elif p[p_idx - 1] == '.':
-                    if p_idx < p_len - 1 and p[p_idx + 1] == s[s_idx]:
-                        s_idx += 1
-                        p_idx += 1
-                    # elif p_idx < p_len - 1 and p[p_idx + 1] == '.':
-                    #     p_idx += 1
-                    else:
-                        s_idx += 1
-                elif p[p_idx - 1] == s[s_idx]:
+                elif p_context_prev == s[s_idx]:
                     s_idx += 1
                 else:
                     p_idx += 1
+                    p_context_prev = '*'
+                    p_context_cur = self.get(p, p_idx)
             else:
-                if s[s_idx] == p[p_idx]:
+                if s[s_idx] == p_context_cur:
                     s_idx += 1
                     p_idx += 1
-                elif p_idx + 1 < p_len and p[p_idx + 1] == '*':
+                    p_context_prev = p_context_cur
+                    p_context_cur = self.get(p, p_idx)
+                elif self.get(p, p_idx + 1) == '*':
                     p_idx += 2
+                    p_context_prev = '*'
+                    p_context_cur = self.get(p, p_idx)
                 else:
                     return False
 
         return s_idx >= s_len and (p_idx >= p_len - 1 or self.emptyExpressionLeft(p, p_idx, len(p)))
+
+    def get(self, arr, i):
+        if len(arr) > i: return arr[i]
 
     def emptyExpressionLeft(self, p: str, start: int, end: int):
         while start < end:
@@ -52,13 +71,13 @@ class Solution:
 
 
 s = Solution()
-# s.isMatchWithPrint("1", '1*')
-# s.isMatchWithPrint("1", '1.*')
-# s.isMatchWithPrint("aa", "a")
-# s.isMatchWithPrint("aa", "a*")
-# s.isMatchWithPrint("ab", ".*")
-# s.isMatchWithPrint("aab", "c*a*b")
-# s.isMatchWithPrint("mississippi", "mis*is*p*.")
+s.isMatchWithPrint("1", '1*')
+s.isMatchWithPrint("1", '1.*')
+s.isMatchWithPrint("aa", "a")
+s.isMatchWithPrint("aa", "a*")
+s.isMatchWithPrint("ab", ".*")
+s.isMatchWithPrint("aab", "c*a*b")
+s.isMatchWithPrint("mississippi", "mis*is*p*.")
 s.isMatchWithPrint("aaaaaaaaaaaaabq", ".*.q")
-# s.isMatchWithPrint("abc", ".*.*.*")
-# s.isMatchWithPrint("hello", ".*")
+s.isMatchWithPrint("abc", ".*.*.*")
+s.isMatchWithPrint("hello", ".*")

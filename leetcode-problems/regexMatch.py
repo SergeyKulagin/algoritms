185ba2ab34361a55b22163eaea274a41a67706d0
sergey.kulagin <sergey.kulagin@synesis-sport.by>
2020-07-24 00:22:32 +0300

    [leetcode]: regexp matching - test against lib regexp matcher

--- leetcode-problems/regexMatch.py
+++ leetcode-problems/regexMatch.py
@@ -1,5 +1,7 @@
 # see also https://regex101.com/
 
+import re
+
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         s_idx = 0
@@ -24,16 +26,18 @@ class Solution:
                     return True
 
                 s_back = s_len - 1 # go backward for search for last inclusion
+                found = False
                 while s_back >= s_idx:
                     if self.get(p, p_idx + 1) == s[s_back]:
                         p_idx += 2
                         s_idx = s_back + 1
                         p_context_cur = self.get(p, p_idx)
                         p_context_prev = self.get(s, s_back)
+                        found = True
                         break
                     else:
                         s_back -= 1
-                if s_back == s_idx:
+                if not found:
                     return False
             elif p_context_cur == '*':
                 if p_context_prev is None or p_context_prev == '*':
@@ -82,29 +86,30 @@ class Solution:
                     return False
         return True
 
-    def isMatchWithPrint(self, s: str, p: str, expected: bool) -> bool:
+    def isMatchWithPrint(self, s: str, p: str) -> bool:
         match = self.isMatch(s, p)
+
         print(s + " is mathes to " + p + " = " + str(match))
-        assert match == expected, "Not expected"
+        assert match == bool(re.fullmatch(p, s)), "Not expected"
 
 
 s = Solution()
-s.isMatchWithPrint("1", '1*', True)
-s.isMatchWithPrint("11111111111111", '1*', True)
-s.isMatchWithPrint("1", '1.*', True)
-s.isMatchWithPrint("aa", "a", False)
-s.isMatchWithPrint("aa", "a*", True)
-s.isMatchWithPrint("ab", ".*", True)
-s.isMatchWithPrint("aab", "c*a*b", True)
-s.isMatchWithPrint("mississippi", "mis*is*p*.", False)
-s.isMatchWithPrint("aaaaaaaaaaaaabq", ".*.q", True)
-s.isMatchWithPrint("abc", ".*.*.*", True)
-s.isMatchWithPrint("hello", ".*", True)
-s.isMatchWithPrint("", "", True)
-s.isMatchWithPrint("", ".*", True)
-s.isMatchWithPrint("qqwqrwe", ".*", True)
-s.isMatchWithPrint("dsfsdsafdfd", ".*a.*", True)
-s.isMatchWithPrint("dsfsdsfdfd", ".*a.*", False)
-s.isMatchWithPrint("aaabbbccca", "a.*b", False)
-s.isMatchWithPrint("aaabbbccca", "a.*", True)
-s.isMatchWithPrint("aaabbbccca", "a.*a", True)
+s.isMatchWithPrint("1", '1*')
+s.isMatchWithPrint("11111111111111", '1*')
+s.isMatchWithPrint("1", '1.*')
+s.isMatchWithPrint("aa", "a")
+s.isMatchWithPrint("aa", "a*")
+s.isMatchWithPrint("ab", ".*")
+s.isMatchWithPrint("aab", "c*a*b")
+s.isMatchWithPrint("mississippi", "mis*is*p*.")
+s.isMatchWithPrint("aaaaaaaaaaaaabq", ".*.q")
+s.isMatchWithPrint("abc", ".*.*.*")
+s.isMatchWithPrint("hello", ".*")
+s.isMatchWithPrint("", "")
+s.isMatchWithPrint("", ".*")
+s.isMatchWithPrint("qqwqrwe", ".*")
+s.isMatchWithPrint("dsfsdsafdfd", ".*a.*")
+s.isMatchWithPrint("dsfsdsfdfd", ".*a.*")
+s.isMatchWithPrint("aaabbbccca", "a.*b")
+s.isMatchWithPrint("aaabbbccca", "a.*")
+s.isMatchWithPrint("aaabbbccca", "a.*a")

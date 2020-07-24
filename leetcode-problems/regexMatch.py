8ef435ad9c36124c2aec9ca82e3f9bf560b12f48
sergey.kulagin <sergey.kulagin@synesis-sport.by>
2020-07-24 17:25:38 +0300

    [leetcode]: regexp matching - more cases

--- leetcode-problems/regexMatch.py
+++ leetcode-problems/regexMatch.py
@@ -2,6 +2,7 @@
 
 import re
 
+
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         s_idx = 0
@@ -25,7 +26,7 @@ class Solution:
                 if self.get(p, p_idx + 1) is None:
                     return True
 
-                s_back = s_len - 1 # go backward for search for last inclusion
+                s_back = s_len - 1  # go backward for search for last inclusion
                 found = False
                 while s_back >= s_idx:
                     if self.get(p, p_idx + 1) == s[s_back]:
@@ -41,8 +42,10 @@ class Solution:
                     return False
             elif p_context_cur == '*':
                 if p_context_prev is None or p_context_prev == '*':
-                    raise RuntimeError('Invalid pattern')
+                    raise RuntimeError(f'Invalid pattern {p}')
                 elif p_context_prev == s[s_idx]:
+                    while self.get(p, p_idx + 1) == p_context_prev:
+                        p_idx += 2
                     s_idx += 1
                 else:
                     p_idx += 1
@@ -61,7 +64,7 @@ class Solution:
                 else:
                     return False
 
-        return s_idx >= s_len and (p_idx >= p_len - 1 or self.emptyExpressionLeft(p, p_idx))
+        return s_idx >= s_len and (p_idx >= p_len or self.emptyExpressionLeft(p, p_idx))
 
     def get(self, arr, i):
         if len(arr) > i: return arr[i]
@@ -73,7 +76,7 @@ class Solution:
         while p_idx < p_len:
             if p_cur == '*':
                 if p_prev == '*':
-                    raise RuntimeError('Invalid pattern')
+                    raise RuntimeError(f'Invalid pattern {p}')
                 p_prev = p_cur
                 p_idx += 1
                 p_cur = self.get(p, p_idx)
@@ -89,18 +92,45 @@ class Solution:
     def isMatchWithPrint(self, s: str, p: str) -> bool:
         match = self.isMatch(s, p)
 
-        print(s + " is mathes to " + p + " = " + str(match))
+        # print(s + " is mathes to " + p + " = " + str(match))
+        print('"' + s + '"')
+        print('"' + p + '"')
         assert match == bool(re.fullmatch(p, s)), "Not expected"
 
 
 s = Solution()
-s.isMatchWithPrint("1", '1*')
-s.isMatchWithPrint("11111111111111", '1*')
-s.isMatchWithPrint("1", '1.*')
-s.isMatchWithPrint("aa", "a")
-s.isMatchWithPrint("aa", "a*")
-s.isMatchWithPrint("ab", ".*")
+#exact match
+s.isMatchWithPrint("abc", "abc")
+s.isMatchWithPrint("abc", "abcc")
+s.isMatchWithPrint("abcd", "abc")
+s.isMatchWithPrint("a", "")
+s.isMatchWithPrint("", "a")
+# expressions (star)
+s.isMatchWithPrint("a", 'a*')
+s.isMatchWithPrint("aaaaa", 'a*')
+s.isMatchWithPrint("aaaaa", 'a*a*')
+s.isMatchWithPrint("aaaaa", 'a*a*a*a*a*')
+s.isMatchWithPrint("aaaaa", 'b*')
 s.isMatchWithPrint("aab", "c*a*b")
+# expressions (dot)
+s.isMatchWithPrint("a", ".")
+s.isMatchWithPrint("a", "..")
+s.isMatchWithPrint("a", "a.")
+s.isMatchWithPrint("a", "a..")
+s.isMatchWithPrint("aaa", "...")
+s.isMatchWithPrint("aaa", "....")
+s.isMatchWithPrint("aaa", ".a.")
+# expressions (dot and match)
+s.isMatchWithPrint("1", '1.*')
+s.isMatchWithPrint("1", '1.*.*.*')
+s.isMatchWithPrint("1", '1.*.*.')
+s.isMatchWithPrint("1", '1.*.*.')
+s.isMatchWithPrint("1b1", '1.*.*1')
+s.isMatchWithPrint("1b1c1", '1.*.*1')
+s.isMatchWithPrint("1b1c1", '1.*.*1')
+s.isMatchWithPrint("1b1sadfsafsdfasc1", '.*')
+
+# misc
 s.isMatchWithPrint("mississippi", "mis*is*p*.")
 s.isMatchWithPrint("aaaaaaaaaaaaabq", ".*.q")
 s.isMatchWithPrint("abc", ".*.*.*")
@@ -113,3 +143,5 @@ s.isMatchWithPrint("dsfsdsfdfd", ".*a.*")
 s.isMatchWithPrint("aaabbbccca", "a.*b")
 s.isMatchWithPrint("aaabbbccca", "a.*")
 s.isMatchWithPrint("aaabbbccca", "a.*a")
+s.isMatchWithPrint("aaa", "a*a")
+s.isMatchWithPrint("aaa", "ab*a*c*a")
